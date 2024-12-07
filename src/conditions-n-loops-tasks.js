af660086291fa4d8d3ed099911d3005271e13475
V1ADDD <rusakvlad2@gmail.com>
2024-12-07 14:55:01 +0300

    feat: 16/19

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -475,9 +475,26 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let evenChars = '';
+    let oddChars = '';
+
+    for (let j = 0; j < result.length; j += 1) {
+      if (j % 2 === 0) {
+        evenChars += result[j];
+      } else {
+        oddChars += result[j];
+      }
+    }
+    result = evenChars + oddChars;
+  }
+
+  return result;
 }
+
 
 /**
  * Returns the nearest largest integer consisting of the digits of the given positive integer.
